034e8c232200f9b3b47a564dd73be6ad12d9cec1
katheryn-k <110975029+kate-its-kate@users.noreply.github.com>
2022-09-18 22:01:08 +0300

    add: html layout and styles

---
+++ app.js
@@ -0,0 +1,82 @@
+const quizData = [
+    {
+        question: 'What is the smallest planet in our solar system?',
+        a: 'Venus',
+        b: 'Jupiter',
+        c: 'Mercury',
+        d: 'Earth',
+        correct: 'c'
+    },
+    {
+        question: 'Name the Coffee shop in US sitcom Friends:',
+        a: 'Friends',
+        b: 'Central Perk',
+        c: 'Coffee with Friends',
+        d: 'Central Park',
+        correct: 'b'
+    },
+    {
+        question: 'What is the capital of New Zealand?',
+        a: 'Rotorua',
+        b: 'Auckland',
+        c: 'Queenstown',
+        d: 'Wellington',
+        correct: 'd'
+    },
+    {
+        question: 'What was the old name for a Snickers bar before it changed in 1990?',
+        a: 'Marathon bars',
+        b: 'Mars bars',
+        c: 'Speed bars',
+        d: 'Run bars',
+        correct: 'a'
+    },
+    {
+        question: 'What is the currency of Denmark?',
+        a: 'Euro',
+        b: 'Dollar',
+        c: 'Pound',
+        d: 'Krone',
+        correct: 'd'
+    },
+    {
+        question: 'What band was Harry Styles in before his solo career?',
+        a: 'One Direction',
+        b: 'White Eskimo',
+        c: 'The boys',
+        d: 'None',
+        correct: ''
+    },
+    {
+        question: 'How many permanent teeth does a dog have?',
+        a: '42',
+        b: '34',
+        c: '32',
+        d: '27',
+        correct: 'a'
+    },
+    {
+        question: 'Which color pill does Neo swallow in The Matrix?',
+        a: 'Green',
+        b: 'Blue',
+        c: 'Red',
+        d: 'White',
+        correct: 'c'
+    },
+    {
+        question: 'What does Bridget Jones name her baby in the film series’s third instalment?',
+        a: 'William',
+        b: 'Ben',
+        c: 'Richard',
+        d: 'Robert',
+        correct: 'a'
+    },
+    {
+        question: 'Gouda is a popular cheese originating from which country?',
+        a: 'Hungary',
+        b: 'Austria',
+        c: 'France',
+        d: 'Netherlands',
+        correct: 'd'
+    }
+];
